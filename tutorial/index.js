bb7b12ef5270bbd602c6e960a68817f694733600
germang2 <germandg99@gmail.com>
2017-10-22 21:58:17 -0500

    It adds mongoDb and mongoose, conection fail

--- tutorial/index.js
+++ tutorial/index.js
@@ -1,6 +1,28 @@
 var express = require("express");
-
 var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/fotos',{ useMongoClient: true });
+
+var Schema = mongoose.Schema;
+
+// This userSchemaJSON is like a table in mongoDB
+var userSchemaJSON = {
+	email:String,
+	password:String
+};
+
+// This class create an object or document in mongoDB
+var user_schema = new Schema(userSchemaJSON);
+
+var User = mongoose.model("User", user_schema);
+
+//This middleware of express set the folder for static files
+app.use(express.static('public'));
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "jade");
 
@@ -11,6 +33,13 @@ app.get("/", function(req, res){
 
 app.get("/login", function(req, res){
 	res.render("login");
+});
+
+app.post("/users", function(req, res){
+	var user = new User({email: req.body.email, password: req.body.password});
+	user.save(function(){
+		res.send("Guardamos tus datos");
+	});
 });
 
 app.listen(8080);
